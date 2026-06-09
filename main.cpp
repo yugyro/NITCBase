1ddb644148fab75cee99865b898aa99434a6467c
Yugyro <fadilhasannishad@gmail.com>
2026-06-10 01:05:05 +0530

    Stage 4:TODO OpenRelTable

--- main.cpp
+++ main.cpp
@@ -9,44 +9,33 @@
 int main(int argc, char *argv[]) {
   Disk disk_run;
   StaticBuffer buffer;
-  // create objects for the relation catalog and attribute catalog
-  RecBuffer relCatBuffer(RELCAT_BLOCK);
+  OpenRelTable cache;
 
-  HeadInfo relCatHeader;
+  /*
+  for i = 0 and i = 1 (i.e RELCAT_RELID and ATTRCAT_RELID)
 
-  // load the headers of both the blocks into relCatHeader and attrCatHeader.
-  // (we will implement these functions later)
-  relCatBuffer.getHeader(&relCatHeader);
+      get the relation catalog entry using RelCacheTable::getRelCatEntry()
+      printf("Relation: %s\n", relname);
 
-  for (int i=0;i<relCatHeader.numEntries;i++) {
-    Attribute relCatRecord[RELCAT_NO_ATTRS]; // will store the record from the relation catalog
+      for j = 0 to numAttrs of the relation - 1
+          get the attribute catalog entry for (rel-id i, attribute offset j)
+           in attrCatEntry using AttrCacheTable::getAttrCatEntry()
 
-    relCatBuffer.getRecord(relCatRecord, i);
+          printf("  %s: %s\n", attrName, attrType);
+  */ 
+  for(int i=0;i<2;i++)
+  {
+    RelCatEntry relCatEntry;
+    int ret=RelCacheTable::getRelCatEntry(i,&relCatEntry);
+    printf("Relation: %s\n", relCatEntry.relName);
 
-    printf("Relation: %s\n", relCatRecord[RELCAT_REL_NAME_INDEX].sVal);
-
-    int curr=ATTRCAT_BLOCK;
-    RecBuffer attrCatBuffer(curr);
-    HeadInfo attrCatHeader;
-    attrCatBuffer.getHeader(&attrCatHeader);
-    while(curr!=-1)
+    for(int j=0;j<relCatEntry.numAttrs;j++)
     {
-      for (int j=0;j<attrCatHeader.numEntries;j++) {
-        // declare attrCatRecord and load the attribute catalog entry into it
-        Attribute attrCatRecord[ATTRCAT_NO_ATTRS];
-        attrCatBuffer.getRecord(attrCatRecord,j);
-
-        if (strcmp(attrCatRecord[ATTRCAT_REL_NAME_INDEX].sVal,relCatRecord[RELCAT_REL_NAME_INDEX].sVal)==0) {
-          const char *attrType = attrCatRecord[ATTRCAT_ATTR_TYPE_INDEX].nVal == NUMBER ? "NUM" : "STR";
-          printf("  %s: %s\n", attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal, attrType);
-        }
-      }
-      printf("\n");
-      curr=attrCatHeader.rblock;
+      AttrCatEntry attcatbuf;
+      int ret2=AttrCacheTable::getAttrCatEntry(0,j,&attcatbuf);
+      printf("  %s: %s\n", attcatbuf.attrName, attcatbuf.attrType);
     }
-    printf("\n");
   }
-
 
   return 0;
 }
